5eefe2e043387785dac13dfebf2f192c947829a0
Stonkszain <85150052+Stonkszain@users.noreply.github.com>
2023-08-29 14:43:30 +0100

    Time: 612 ms (67.81%), Space: 30.5 MB (44.52%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,40 +1,11 @@
-import sys
-
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        #bruteforce approach
-        
-        # max_sum = -sys.maxsize
-        # for i in range(len(nums)):
-        #     for j in range(i + 1, len(nums) + 1):
-        #         subarray_sum = 0
-        #         for k in nums[i:j]:
-        #             subarray_sum += k
-        #         if subarray_sum > max_sum:
-        #             max_sum = subarray_sum
-                
-        # return max_sum
-
-        # quadratic method
-        # max_sum = -sys.maxsize
-
-        # for i in range(len(nums)):
-        #     subarray_sum = 0
-        #     for j in range(i, len(nums)):
-        #         subarray_sum += nums[j]
-        #         if subarray_sum > max_sum:
-        #             max_sum = subarray_sum
-        # return max_sum
-
-        # O(n) method
         
         max_sum = nums[0]
         cur_sum = 0
 
         for i in nums:
-            if cur_sum < 0:
-                cur_sum = 0
-            cur_sum += i
+            cur_sum = max(cur_sum + i, i)
             max_sum = max(max_sum, cur_sum)
 
         return max_sum
